1f3ef2433a8881884f97b6e83e9029a6aaace78d
Luke <lukewilliams10101@gmail.com>
2024-11-06 11:55:28 +0100

    feat: form validations

---
+++ src/util/forms/formValidation.mjs
@@ -0,0 +1,78 @@
+import setValidationState from "./setValidationState.mjs";
+
+const validateName = (name) => {
+  const namePattern = /^[a-zA-Z0-9_]+$/;
+
+  if (name === "" || !namePattern.test(name) || name.length > 20) {
+    setValidationState("#floatingName", false);
+    return false;
+  } else {
+    setValidationState("#floatingName", true);
+    return true;
+  }
+};
+
+const validateEmail = (email) => {
+  const emailPattern = /^[a-z0-9._%+-]+@(noroff\.no|stud\.noroff\.no)$/i;
+
+  if (email === "") {
+    setValidationState("#floatingEmail", false);
+    return false;
+  } else if (!emailPattern.test(email)) {
+    setValidationState("#floatingEmail", false);
+    return false;
+  } else {
+    setValidationState("#floatingEmail", true);
+    return true;
+  }
+};
+
+const validatePassword = (password) => {
+  if (password === "") {
+    setValidationState("#floatingPassword", false);
+    return false;
+  } else if (password.length < 8) {
+    setValidationState("#floatingPassword", false);
+    return false;
+  } else {
+    setValidationState("#floatingPassword", true);
+    return true;
+  }
+};
+
+const validateConfirmPassword = (password, confirmPassword) => {
+  if (confirmPassword === "") {
+    setValidationState("#floatingConfirmPassword", false);
+    return false;
+  } else if (confirmPassword !== password) {
+    setValidationState("#floatingConfirmPassword", false);
+    return false;
+  } else {
+    setValidationState("#floatingConfirmPassword", true);
+    return true;
+  }
+};
+
+/**
+ * Checks if signup form data is valid.
+ * @param {string} name
+ * @param {string} email
+ * @param {string} password
+ * @param {string} confirmPassword
+ * @returns {boolean}
+ */
+export const validateSignupFormData = (
+  name,
+  email,
+  password,
+  confirmPassword
+) => {
+  let isValid = true;
+
+  if (!validateName(name)) isValid = false;
+  if (!validateEmail(email)) isValid = false;
+  if (!validatePassword(password)) isValid = false;
+  if (!validateConfirmPassword(password, confirmPassword)) isValid = false;
+
+  return isValid;
+};
