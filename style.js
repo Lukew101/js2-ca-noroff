b62020fe7e386dacc5a232362dd3edc29fa616f3
Luke <lukewilliams10101@gmail.com>
2024-11-06 11:55:59 +0100

    feat: minor improvements

--- style.js
+++ style.js
@@ -1,11 +1,23 @@
-// document
-//   .querySelector("#loginForm")
-//   .addEventListener("submit", function (event) {
-//     event.preventDefault();
-//     if (this.checkValidity()) {
-//       window.location.href = "/profile/index.html";
-//     }
-//   });
+import login from "./src/util/forms/login.mjs";
+
+const loginForm = document.querySelector("#loginForm");
+
+document.addEventListener("DOMContentLoaded", function () {
+  loginForm.addEventListener("submit", function (event) {
+    const email = document.querySelector("#floatingEmail").value.trim();
+    const password = document.querySelector("#floatingPassword").value;
+
+    const isFormValid = loginForm.checkValidity();
+
+    if (!isFormValid) {
+      event.preventDefault();
+    } else {
+      const requestData = { email, password };
+      login(requestData);
+      event.preventDefault();
+    }
+  });
+});
 
 document
   .querySelector("#registerButton")
