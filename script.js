b0c194ae76f04c3374262b6a7cbbc156947de984
Luke <lukewilliams10101@gmail.com>
2024-11-06 14:41:44 +0100

    feat: handle logout

---
+++ script.js
@@ -0,0 +1,27 @@
+const isUserLoggedIn = () => {
+  const accessToken = localStorage.getItem("accessToken");
+  return accessToken !== null;
+};
+
+const handleLoggedInUser = () => {
+  const isLoggedIn = isUserLoggedIn();
+  console.log(isLoggedIn);
+  if (isLoggedIn) {
+    const navbar = document.querySelector(".navbar-nav");
+    const signOut = document.createElement("a");
+    signOut.classList.add("nav-link");
+    signOut.classList.add("signout-nav-link");
+    signOut.classList.add("fs-5");
+    signOut.innerHTML = "Sign out";
+    signOut.href = "/";
+    navbar.appendChild(signOut);
+    signOut.addEventListener("click", function () {
+      localStorage.removeItem("accessToken");
+      localStorage.removeItem("profile");
+    });
+  } else {
+    document.location.href = "/";
+  }
+};
+
+handleLoggedInUser();
