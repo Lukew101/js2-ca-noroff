357024ee68694f7203094ce40b47a7842d9e051c
Luke <lukewilliams10101@gmail.com>
2024-11-07 13:41:22 +0100

    feat: correct profile picture

--- script.js
+++ script.js
@@ -6,14 +6,22 @@ const isUserLoggedIn = () => {
 const handleLoggedInUser = () => {
   const isLoggedIn = isUserLoggedIn();
   if (isLoggedIn) {
+    // Display sign out button
     const navbar = document.querySelector(".navbar-nav");
     const signOut = document.createElement("a");
-    signOut.classList.add("nav-link");
-    signOut.classList.add("signout-nav-link");
-    signOut.classList.add("fs-5");
+    signOut.classList.add("nav-link", "signout-nav-link", "fs-5");
     signOut.innerHTML = "Sign out";
     signOut.href = "/";
     navbar.appendChild(signOut);
+
+    // Display profile picture
+    const profilePicture = document.querySelectorAll(".profile-pic");
+    const profile = JSON.parse(localStorage.getItem("profile"));
+    profilePicture.forEach((pic) => {
+      pic.src = profile.avatar.url;
+      pic.alt = profile.avatar.alt;
+    });
+
     signOut.addEventListener("click", function () {
       localStorage.removeItem("accessToken");
       localStorage.removeItem("profile");
